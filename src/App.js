0a8f67944c79a30f43e42cd926da8f64e164793b
Doron Chosnek <my.random.items@gmail.com>
2024-10-07 22:54:49 -0500

    added components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import JsonForm from './components/JsonForm/JsonForm';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
+
+  const avatarUrl = 'https://i.pravatar.cc/50';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Navbar inside a Container */}
+      <Container>
+        {/* <Navbar bg="dark" variant="dark" className="mt-3 rounded">
+          <Navbar.Brand href="#home">Webex Blaster</Navbar.Brand>
+        </Navbar> */}
+        <Navbar avatarUrl={avatarUrl} />
+      </Container>
+      
+      {/* Main Container */}
+      <Container className="mt-4">
+        <Row className="justify-content-md-center">
+          <Col md="8">
+            <h1>Welcome to Webex Blaster</h1>
+            This is the app that will send Adaptive Cards in Webex on your behalf.
+            <hr width="100%" size="4" color="blue"></hr>
+            {/* Form Component */}
+            <JsonForm />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
