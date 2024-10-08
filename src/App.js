c6d4af8350b08d81056e298c12b9796264404848
Doron Chosnek <my.random.items@gmail.com>
2024-10-08 08:57:14 -0500

    retrieve avatarUrl from query param

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import JsonForm from './components/JsonForm/JsonForm';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
 
-  const avatarUrl = 'https://i.pravatar.cc/50';
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  // State to store the avatar URL, starting with a default value
+  const [avatarUrl, setAvatarUrl] = useState('');
+  
+  // useEffect to extract the query parameter on component mount
+  useEffect(() => {
+    // Get the current URL query parameters
+    const queryParams = new URLSearchParams(window.location.search);
+    
+    // Check if `avatarUrl` exists in the URL and update the state if it does
+    const urlAvatar = queryParams.get('avatarUrl');
+    if (urlAvatar) {
+      setAvatarUrl(urlAvatar);
+      
+      setIsAuthenticated(true);
+
+      // Use history.replaceState to update the URL without reloading
+      window.history.replaceState({}, '', `${window.location.pathname}`);
+    }
+  }, []); // Empty dependency array ensures this effect only runs once on mount
+
 
   return (
     <div>
       {/* Navbar inside a Container */}
       <Container>
-        {/* <Navbar bg="dark" variant="dark" className="mt-3 rounded">
-          <Navbar.Brand href="#home">Webex Blaster</Navbar.Brand>
-        </Navbar> */}
-        <Navbar avatarUrl={avatarUrl} />
+        {/* Navbar Component */}
+        <Navbar isAuthenticated={isAuthenticated} avatarUrl={avatarUrl}/>
       </Container>
       
       {/* Main Container */}
@@ -25,7 +44,7 @@ function App() {
             This is the app that will send Adaptive Cards in Webex on your behalf.
             <hr width="100%" size="4" color="blue"></hr>
             {/* Form Component */}
-            <JsonForm />
+            <JsonForm isAuthenticated={isAuthenticated} />
           </Col>
         </Row>
       </Container>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navbar as BootstrapNavbar, Container } from 'react-bootstrap';
 import './Navbar.css'; // Import the custom CSS file for styling
 
-function Navbar({ avatarUrl }) {
+function Navbar({ isAuthenticated, avatarUrl }) {
     return (
         <Container>
             <BootstrapNavbar bg="dark" variant="dark" className="mt-3 rounded px-4 d-flex justify-content-between align-items-center">
@@ -22,11 +22,18 @@ function Navbar({ avatarUrl }) {
                 </div>
 
                 {/* Right Section: Round Avatar Image */}
-                <img
-                    src={ avatarUrl }
-                    alt="User Avatar"
-                    className="avatar"
-                />
+                {isAuthenticated ? (
+                    <img
+                        src={avatarUrl}
+                        alt="User Avatar"
+                        className="avatar"
+                    />
+                ) : (
+                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" className="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                        <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                    </svg>
+                )}
             </BootstrapNavbar>
         </Container>
     );
