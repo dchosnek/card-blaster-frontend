c554ed21f6bafb5a828a75550852f83d2b822adb
Doron Chosnek <my.random.items@gmail.com>
2024-10-10 08:26:03 -0500

    send card logic

--- src/components/JsonForm/JsonForm.js
+++ src/components/JsonForm/JsonForm.js
@@ -4,8 +4,40 @@ import './JsonForm.css'; // Import the CSS file for additional styling
 
 function cardForm({ isAuthenticated }) {
 
+    const handleSubmit = (event) => {
+        event.preventDefault(); // Prevent default form submission
+    
+        // Get values from the form fields
+        const roomId = event.target.elements.SearchText1.value;
+        const card = event.target.elements.Textarea1.value;
+    
+        // Perform a POST request to /sendcard
+        fetch('/sendcard', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            roomId: roomId, // Set the "roomId" field in the payload
+            card: JSON.parse(card), // Convert the card payload string to an object
+          }),
+        })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error('Failed to send card.');
+            }
+            return response.json();
+          })
+          .then((data) => {
+            alert('Card sent successfully!');
+          })
+          .catch((error) => {
+            alert('Failed to send the card.');
+          });
+      };
+
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="Textarea1">
                 <Form.Label>Enter your adaptive card payload below. Use the Webex Adaptive Card <a href="https://developer.webex.com/buttons-and-cards-designer" target="_blank" rel="noreferrer">Designer</a> to create your card.</Form.Label>
                 <Form.Control
