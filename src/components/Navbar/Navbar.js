ea140166fb64c38b0df4618b489d74248833bbc7
Doron Chosnek <my.random.items@gmail.com>
2024-10-10 09:17:08 -0500

    show recent activity menu item

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Navbar as BootstrapNavbar, Container } from 'react-bootstrap';
-import './Navbar.css'; // Import the custom CSS file for styling
+import { Navbar as BootstrapNavbar, Container, Dropdown, DropdownButton } from 'react-bootstrap';
+import './Navbar.css'; 
 
-function Navbar({ isAuthenticated, avatarUrl, nickName }) {
+function Navbar({ isAuthenticated, avatarUrl, nickName, setShowRecentActivity }) {
 
     const handleLoginClick = () => {
         window.location.href = 'http://localhost:3000/login';
@@ -22,6 +22,9 @@ function Navbar({ isAuthenticated, avatarUrl, nickName }) {
             });
     };
 
+    // when user clicks "Recent Activity" set the show state to true for the history modal
+    const handleRecentActivityClick = () => setShowRecentActivity(true);
+
     return (
         <Container>
             <BootstrapNavbar bg="dark" variant="dark" className="mt-3 rounded px-4 d-flex justify-content-between align-items-center">
@@ -29,46 +32,56 @@ function Navbar({ isAuthenticated, avatarUrl, nickName }) {
                 <div className="d-flex align-items-center">
                     {/* Blaster Icon */}
                     <img
-                        src={`${process.env.PUBLIC_URL}/blaster.png`} // Use the public path to access blaster.png
+                        src={`${process.env.PUBLIC_URL}/blaster.png`}
                         alt="Blaster Icon"
-                        className="blaster-icon" // Apply custom styling for the blaster icon
+                        className="blaster-icon"
                     />
 
                     {/* Brand Name */}
-                    <BootstrapNavbar.Brand href="#home" className="ms-2"> {/* Add left margin */}
+                    <BootstrapNavbar.Brand href="#home" className="ms-2">
                         Webex Blaster
                     </BootstrapNavbar.Brand>
                 </div>
 
-                {/* Right Section: Round Avatar Image */}
+                {/* Right Section: Avatar or SVG with Dropdown Menus */}
                 {isAuthenticated ? (
-                    <div style={{ display: 'flex', alignItems: 'center' }}>
-                    {/* Salutation text */}
-                    <span style={{ marginRight: '10px', fontSize: '16px', color: '#fff' }}>
-                      Hi, {nickName}
-                    </span>
-                    {/* Avatar Image */}
-                    <img
-                      onClick={handleLogoutClick}
-                      style={{ cursor: 'pointer', borderRadius: '50%' }}
-                      src={avatarUrl}
-                      alt="User Avatar"
-                      className="avatar"
-                    />
-                  </div>
+                    <Dropdown align="end">
+                        <Dropdown.Toggle as="div" style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }}>
+                            <span style={{ marginRight: '10px', fontSize: '16px', color: '#fff' }}>Hi, {nickName}</span>
+                            <img
+                                src={avatarUrl}
+                                alt="User Avatar"
+                                style={{ width: '40px', height: '40px', borderRadius: '50%' }}
+                                className="avatar"
+                            />
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item onClick={handleRecentActivityClick}>Recent Activity</Dropdown.Item>
+                            <Dropdown.Item onClick={handleLogoutClick}>Logout</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
                 ) : (
-                    <svg
-                        onClick={handleLoginClick}
-                        style={{ cursor: 'pointer' }}
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="40"
-                        height="40"
-                        fill="white"
-                        className="bi bi-person-circle"
-                        viewBox="0 0 16 16">
-                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                        <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
-                    </svg>
+                    <Dropdown align="end">
+                        <Dropdown.Toggle as="div" style={{ cursor: 'pointer' }}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="40"
+                                height="40"
+                                fill="white"
+                                className="bi bi-person-circle"
+                                viewBox="0 0 16 16"
+                            >
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                                <path
+                                    fillRule="evenodd"
+                                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+                                />
+                            </svg>
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item onClick={handleLoginClick}>Login</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
                 )}
             </BootstrapNavbar>
         </Container>
