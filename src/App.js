3be812212a42d47741a5ce8d1d13939d6e537c4d
Doron Chosnek <my.random.items@gmail.com>
2024-10-13 14:45:27 -0500

    added carousel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Carousel, Container, Row, Col } from 'react-bootstrap';
 import JsonForm from './components/JsonForm/JsonForm';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
@@ -76,8 +76,23 @@ function App() {
                   <h1>Welcome to Card Blaster for Webex</h1>
                   This is the app that will send Adaptive Cards in Webex on your behalf.
                   <hr width="100%" size="4" color="blue"></hr>
-                  <p>Design your card using the Webex Adaptive Card <a href="https://developer.webex.com/buttons-and-cards-designer" target="_blank" rel="noreferrer">Designer</a>. You don't have to understand the JSON that the Designer creates. You just have to copy the JSON from the Designer and paste it into this site to send the card to a person or space in Webex.</p>
-                  <p>Once you've designed a card and are ready to send it, log in via the menu in the navigation bar.</p>
+                  <p><b>Step 1: </b>Design your card using the Webex Adaptive Card <a href="https://developer.webex.com/buttons-and-cards-designer" target="_blank" rel="noreferrer">Designer</a>. You don't have to understand the JSON that the Designer creates. You just have to copy the JSON from the Designer and paste it into Card Blaster.</p>
+                  <p><b>Step 2: </b>Paste the contents from the designer into this tool and choose the Webex room/space to send it to. You can send cards to individuals or group spaces.</p>
+                  <Carousel interval="1500">
+                    <Carousel.Item>
+                      <img src={`${process.env.PUBLIC_URL}/blaster-card.png`} className="d-block mx-auto" border="1"/>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img src={`${process.env.PUBLIC_URL}/weather-card.png`} className="d-block mx-auto" border="1" />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img src={`${process.env.PUBLIC_URL}/webinar-card-light.png`} className="d-block mx-auto" border="1" />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img src={`${process.env.PUBLIC_URL}/restaurant-card.png`} className="d-block mx-auto" border="1" />
+                    </Carousel.Item>
+                  </Carousel>
+                  <p>Howdy.</p>
                 </div>
             }
           </Col>
