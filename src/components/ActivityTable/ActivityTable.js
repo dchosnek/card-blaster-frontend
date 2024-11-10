3b07ebd3c58829aa37be02d379ef6708ddba1cc4
Doron Chosnek <my.random.items@gmail.com>
2024-11-09 21:46:02 -0600

    move row to be its own component

--- src/components/ActivityTable/ActivityTable.js
+++ src/components/ActivityTable/ActivityTable.js
@@ -1,41 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { Modal, Table } from 'react-bootstrap';
+import ActivityRow from '../ActivityRow/ActivityRow';
 import './ActivityTable.css'
 
 function ActivityTable({ show, setShow }) {
 
-  // convert the timestamp to local timezone and format it
-  const formatDate = (timestamp) => {
-    if (!timestamp) return 'N/A'; // Handle missing timestamps
+  // history is the list of maps to display in the table
+  const [history, setHistory] = useState([]);
 
-    const dateObj = new Date(timestamp); // convert to JavaScript Date object
-    return dateObj.toLocaleString([], {
-      year: 'numeric',
-      month: '2-digit',
-      day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-      hour12: true // use AM/PM
-    });
-  };
-
-  // Function to split a string in half
-  const splitStringInHalf = (str) => {
-    let firstHalf, secondHalf;
-    if (str) {
-      const middleIndex = Math.floor(str.length / 2);
-      firstHalf = str.substring(0, middleIndex);
-      secondHalf = str.substring(middleIndex);
-    } else {
-      firstHalf = null;
-      secondHalf = null;
-    }
-    return { firstHalf, secondHalf };
-  };
-
-  // data is the list of maps to display in the table
-  const [data, setData] = useState([]);
+  const [deletedMessages, setDeletedMessages] = useState([]);
 
   // change the state of the the visibility of the modal
   const handleClose = () => setShow(false);
@@ -53,7 +26,9 @@ function ActivityTable({ show, setShow }) {
           return null;
         })
         .then((mylist) => {
-          setData(mylist);
+          setHistory(mylist);
+          // create list containing messageId for deleted messages
+          setDeletedMessages(mylist.filter(x => x.activity==='delete card').map(y => y.messageId));
         })
     }
   }, [show]);
@@ -70,29 +45,14 @@ function ActivityTable({ show, setShow }) {
             <tr>
               <th className="nowrap-column">Timestamp</th>
               <th className="nowrap-column">Activity</th>
-              <th className="nowrap-column">Type</th>
-              <th className="nowrap-column">Result</th>
-              <th className="wrap-column">Message Id</th>
+              <th className="wrap-column">Recipient</th>
+              <th className="nowrap-column">Action</th>
             </tr>
           </thead>
           <tbody>
-            {data.map((entry, index) => {
-              const splitMessage = splitStringInHalf(entry.messageId);
+            {history.map((entry, index) => {
               return (
-                <tr key={index}>
-                  <td className="nowrap-column">{formatDate(entry.timestamp)}</td>
-                  <td className="nowrap-column">{entry.activity}</td>
-                  <td className="nowrap-column">{entry.type}</td>
-                  {/* display success or failure only if that is a valid state */}
-                  <td className="nowrap-column">{entry.success === undefined ? "" : entry.success ? "success" : "fail"}</td>
-                  {/* display messageId split in half so as to make the table readable */}
-                  <td>
-                    <span className="split-text">
-                      {splitMessage.firstHalf}
-                    </span>
-                    {splitMessage.secondHalf}
-                  </td>
-                </tr>
+                <ActivityRow key={index} entry={entry} deletedMessages={deletedMessages} setDeletedMessages={setDeletedMessages}/>
               )
             })}
           </tbody>
