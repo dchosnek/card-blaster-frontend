70e4074da93e4a20a1e77d1614bebaefdc345718
Doron Chosnek <my.random.items@gmail.com>
2024-11-14 21:06:34 -0600

    multiselect

--- src/components/JsonForm/JsonForm.js
+++ src/components/JsonForm/JsonForm.js
@@ -8,8 +8,6 @@ const Joi = require('joi');
 function cardForm({ roomList, sendAlert }) {
 
     // State variables for each form field
-    const [roomId, setRoomId] = useState('');
-    const [roomType, setRoomType] = useState('');
     const [card, setCard] = useState('');
     const [hideSubmit, setHideSubmit] = useState(false);
 
@@ -19,53 +17,65 @@ function cardForm({ roomList, sendAlert }) {
     // State to track whether the card is valid JSON format
     const [validJson, setValidJson] = useState(false);
 
-    const handleSubmit = (event) => {
+    const sendOneCard = (room, card) => {
+        return new Promise((resolve, reject) =>
+
+            // Perform a POST request to send the card
+            fetch('api/v1/card', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    roomId: room.id,
+                    card: JSON.parse(card), // Convert the card payload string to an object
+                    type: room.type,
+                    roomTitle: room.title,
+                }),
+            })
+                .then((response) => {
+                    if (!response.ok) {
+                        throw new Error('Failed to send card.');
+                    }
+                    return response.json();
+                })
+                .then((data) => {
+                    sendAlert(`Card sent successfully to ${room.title}.`, true);
+                    resolve();  // resolve the promise without returning any data
+                })
+                .catch((error) => {
+                    sendAlert(`Failed to send card to ${room.title}! ${error}`, false);
+                    console.error(`Failed to send card to ${room.title}! ${error}`);
+                    resolve();  // resolve the promise without returning any data
+                })
+        );
+    }
+
+    const handleSubmit = async (event) => {
         event.preventDefault();         // Prevent default form submission
-        
+
+        // disable the submit button while cards are being sent        
         setHideSubmit(true);
 
-        // Perform a POST request to send the card
-        fetch('api/v1/card', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                roomId: roomId,         // Set the "roomId" field in the payload
-                card: JSON.parse(card), // Convert the card payload string to an object
-                type: roomType,
-                roomTitle: selected[0].title,
-                roomId: roomId,
-            }),
-        })
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Failed to send card.');
-                }
-                return response.json();
-            })
-            .then((data) => {
-                const roomName = selected[0].title;
-                sendAlert(`Card sent successfully to ${roomName}.`, true);
-                setHideSubmit(false);
-            })
-            .catch((error) => {
-                sendAlert(`Failed to send card! ${error}`, false);
-                setHideSubmit(false);
-            });
+        console.table(selected);
+
+        // send each card individually but asynchronously
+        const promises = selected.map(async (room) => sendOneCard(room, card));
+
+        // wait for all the cards to be sent successfully or fail
+        await Promise.all(promises);
+
+        // reenable the submit button now that cards have been sent
+        setHideSubmit(false);
     };
 
-    // Handler to capture the selected typeahead item and set roomId
-    const handleSelection = (selectedRoom) => {
-        setSelected(selectedRoom);
-
-        if (selectedRoom.length > 0) {
-            const selectedOption = selectedRoom[0];
-            setRoomId(selectedOption.id);
-            setRoomType(selectedOption.type);
-        } else {
-            setRoomId('');
-        }
+    // We *control* the selection of the typeahead box in order to enable
+    // features like initializing the selection box with some values or
+    // only allowing a certain number of rooms to be selected.
+    // The argument to this function is an array, so it can be checked
+    // for length to limit the number of selected rooms.
+    const handleSelection = (mySelections) => {
+        setSelected(mySelections);
     };
 
     const schema = Joi.object({
@@ -74,7 +84,7 @@ function cardForm({ roomList, sendAlert }) {
         version: Joi.number().min(1.2).max(1.3).required(),
         body: Joi.array().min(1).required(),
         actions: Joi.array().optional(),
-      });       // can add .unknown(true) here to allow for more fields
+    });       // can add .unknown(true) here to allow for more fields
 
     useEffect(() => {
         if (card) {
@@ -90,47 +100,49 @@ function cardForm({ roomList, sendAlert }) {
 
     return (
         <div>
-        <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="Textarea1">
-                <Form.Label>Enter your adaptive card payload below. Use the Webex Adaptive Card <a href="https://developer.webex.com/buttons-and-cards-designer" target="_blank" rel="noreferrer">Designer</a> to create your card.</Form.Label>
-                <Form.Control
-                    as="textarea"
-                    rows={5}
-                    placeholder="Card Payload"
-                    className="form-control-monospace"
-                    value={card} // Controlled component binding
-                    onChange={(e) => setCard(e.target.value)} // Update state on change
-                    isInvalid={!validJson && card.length}   // highlight in red
-                    isValid={validJson && card.length}      // highlight in green
-                />
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>Choose a recipient from these {roomList.length ? roomList.length : ""} rooms/spaces</Form.Label>
-                <Typeahead
-                    id="room-selector"      // id is required for typeahead
-                    options={roomList}
-                    placeholder="Choose a room or person (start typing)..."
-                    labelKey="title"
-                    minLength={1}           // characters typed before displaying dropdown
-                    selected={selected}     // control the selected option
-                    onChange={handleSelection} // triggered when an option is selected
-                />
-            </Form.Group>
-            <Button 
-                variant={validJson && roomId.length ? "outline-primary" : "outline-secondary" } 
-                size="lg" 
-                type="submit" 
-                className="mt-3" 
-                disabled={!validJson || !roomId.length || hideSubmit}>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="Textarea1">
+                    <Form.Label>Enter your adaptive card payload below. Use the Webex Adaptive Card <a href="https://developer.webex.com/buttons-and-cards-designer" target="_blank" rel="noreferrer">Designer</a> to create your card.</Form.Label>
+                    <Form.Control
+                        as="textarea"
+                        rows={5}
+                        placeholder="Card Payload"
+                        className="form-control-monospace"
+                        value={card} // Controlled component binding
+                        onChange={(e) => setCard(e.target.value)} // Update state on change
+                        isInvalid={!validJson && card.length}   // highlight in red
+                        isValid={validJson && card.length}      // highlight in green
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Choose a recipient from these {roomList.length ? roomList.length : ""} rooms/spaces</Form.Label>
+                    <Typeahead
+                        multiple
+                        id="room-selector"      // id is required for typeahead
+                        options={roomList}
+                        placeholder="Choose a room or person (start typing)..."
+                        labelKey="title"
+                        // minLength={1}           // characters typed before displaying dropdown
+                        selected={selected}     // control the selected option
+                        onChange={handleSelection} // triggered when an option is selected
+                        clearButton
+                    />
+                </Form.Group>
+                <Button
+                    variant={validJson && selected.length ? "outline-primary" : "outline-secondary"}
+                    size="lg"
+                    type="submit"
+                    className="mt-3"
+                    disabled={!validJson || !selected.length || hideSubmit}>
                     {
                         hideSubmit ? (
-                            <Spinner animation="border" role="status" aria-hidden="true"/>
+                            <Spinner animation="border" role="status" aria-hidden="true" />
                         ) : (
                             'Submit'
                         )
                     }
-            </Button>
-        </Form >
+                </Button>
+            </Form >
         </div>
     );
 }
