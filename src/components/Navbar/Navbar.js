73297d93b93c79e7f67de83ef0ea36e07f520a10
Doron Chosnek <my.random.items@gmail.com>
2024-12-06 18:25:35 -0600

    replace avatar and add menu item

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Navbar as BootstrapNavbar, Container, Dropdown } from 'react-bootstrap';
+import { PersonFill } from 'react-bootstrap-icons';
 import './Navbar.css'; 
 
-function Navbar({ isAuthenticated, avatarUrl, nickName, setShowRecentActivity, handleLoginClick }) {
+function Navbar({ isAuthenticated, avatarUrl, nickName, setShowRecentActivity, setShowUploadedImages, handleLoginClick }) {
 
     const handleLogoutClick = () => {
         // Make a request to the server-side /logout endpoint
@@ -20,6 +21,8 @@ function Navbar({ isAuthenticated, avatarUrl, nickName, setShowRecentActivity, h
 
     // when user clicks "Recent Activity" set the show state to true for the history modal
     const handleRecentActivityClick = () => setShowRecentActivity(true);
+    // when user clicks "Uploaded Images" set the show state to true for the image list modal
+    const handleUploadedImagesClick = () => setShowUploadedImages(true);
 
     return (
         <Container>
@@ -52,27 +55,19 @@ function Navbar({ isAuthenticated, avatarUrl, nickName, setShowRecentActivity, h
                             />
                         </Dropdown.Toggle>
                         <Dropdown.Menu>
-                            <Dropdown.Item onClick={handleRecentActivityClick}>Recent Activity</Dropdown.Item>
+                            <Dropdown.Item onClick={handleRecentActivityClick}>Sent Cards</Dropdown.Item>
+                            <Dropdown.Item onClick={handleUploadedImagesClick}>Uploaded Images</Dropdown.Item>
                             <Dropdown.Item onClick={handleLogoutClick}>Logout</Dropdown.Item>
                         </Dropdown.Menu>
                     </Dropdown>
                 ) : (
                     <Dropdown align="end">
                         <Dropdown.Toggle as="div" style={{ cursor: 'pointer' }}>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="40"
-                                height="40"
-                                fill="white"
-                                className="bi bi-person-circle"
-                                viewBox="0 0 16 16"
-                            >
-                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                                <path
-                                    fillRule="evenodd"
-                                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
-                                />
-                            </svg>
+                            <PersonFill 
+                                color="white"
+                                style={{ width: '40px', height: '40px', borderRadius: '50%' }}
+                                className="avatar"
+                            />
                         </Dropdown.Toggle>
                         <Dropdown.Menu>
                             <Dropdown.Item onClick={handleLoginClick}>Login</Dropdown.Item>
